1ee62fd54a8c98a851776f0a907271f85c5835f1
roniii12 <ronziyoni@gmail.com>
2021-07-05 07:53:43 +0300

    first commit

---
+++ DogBaeberShop-Server-main/DogBarberShopProject/Migrations/20210704205745_spGetUserById2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DogBarberShopProject.Migrations
+{
+    public partial class spGetUserById2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string procedure = @"create procedure spGetUserById
+                                    @id int
+                                    as
+                                    begin
+                                    select * from users where Id = @id
+                                    end";
+            migrationBuilder.Sql(procedure);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
